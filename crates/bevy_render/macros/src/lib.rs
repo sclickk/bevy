750c0f5626bf0e2f798ec8cd0b539a532d6ed65f
Eric Villarreal <35571755+sclickk@users.noreply.github.com>
2022-06-26 13:09:44 -0500

    use `unwrap_or` instead of `unwrap_or_else`

--- crates/bevy_render/macros/src/lib.rs
+++ crates/bevy_render/macros/src/lib.rs
@@ -7,7 +7,7 @@ pub(crate) fn bevy_render_path() -> syn::Path {
 	BevyManifest::default()
 		.maybe_get_path("bevy_render")
 		// NOTE: If the derivation is within bevy_render, then we need to return 'crate'
-		.unwrap_or_else(|| BevyManifest::parse_str("crate"))
+		.unwrap_or(BevyManifest::parse_str("crate"))
 }
 
 #[proc_macro_derive(ExtractResource)]
