27eba07c28405917822b0de4d6b7e5cdac5e1693
Eric Villarreal <35571755+sclickk@users.noreply.github.com>
2022-07-11 07:20:13 -0500

    formatting

--- crates/bevy_hierarchy/src/events.rs
+++ crates/bevy_hierarchy/src/events.rs
@@ -6,27 +6,27 @@ use bevy_ecs::prelude::Entity;
 /// [`Event`]: bevy_ecs::event::Event
 #[derive(Debug, Clone)]
 pub enum HierarchyEvent {
-    /// Fired whenever an [`Entity`] is added as a child to a new parent.
-    ChildAdded {
-        /// The child that added
-        child: Entity,
-        /// The parent the child was added to
-        parent: Entity,
-    },
-    /// Fired whenever an child [`Entity`] is removed from is parent.
-    ChildRemoved {
-        /// The child that removed
-        child: Entity,
-        /// The parent the child was removed from
-        parent: Entity,
-    },
-    /// Fired whenever an child [`Entity`] is moved to a new parent.
-    ChildMoved {
-        /// The child that moved
-        child: Entity,
-        /// The parent the child was removed from
-        previous_parent: Entity,
-        /// The parent the child was added to
-        new_parent: Entity,
-    },
+	/// Fired whenever an [`Entity`] is added as a child to a new parent.
+	ChildAdded {
+		/// The child that added
+		child: Entity,
+		/// The parent the child was added to
+		parent: Entity,
+	},
+	/// Fired whenever an child [`Entity`] is removed from is parent.
+	ChildRemoved {
+		/// The child that removed
+		child: Entity,
+		/// The parent the child was removed from
+		parent: Entity,
+	},
+	/// Fired whenever an child [`Entity`] is moved to a new parent.
+	ChildMoved {
+		/// The child that moved
+		child: Entity,
+		/// The parent the child was removed from
+		previous_parent: Entity,
+		/// The parent the child was added to
+		new_parent: Entity,
+	},
 }
