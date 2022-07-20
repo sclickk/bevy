fdb0e625dda0540c436df8ad7be211b2323003ce
Eric Villarreal <35571755+sclickk@users.noreply.github.com>
2022-07-20 17:33:50 -0500

    formatting

--- crates/bevy_ui/src/entity.rs
+++ crates/bevy_ui/src/entity.rs
@@ -90,37 +90,37 @@ pub struct TextBundle {
 }
 
 impl TextBundle {
-    /// Create a [`TextBundle`] from a single section.
-    ///
-    /// See [`Text::from_section`] for usage.
-    pub fn from_section(value: impl Into<String>, style: TextStyle) -> Self {
-        Self {
-            text: Text::from_section(value, style),
-            ..Default::default()
-        }
-    }
+	/// Create a [`TextBundle`] from a single section.
+	///
+	/// See [`Text::from_section`] for usage.
+	pub fn from_section(value: impl Into<String>, style: TextStyle) -> Self {
+		Self {
+			text: Text::from_section(value, style),
+			..Default::default()
+		}
+	}
 
-    /// Create a [`TextBundle`] from a list of sections.
-    ///
-    /// See [`Text::from_sections`] for usage.
-    pub fn from_sections(sections: impl IntoIterator<Item = TextSection>) -> Self {
-        Self {
-            text: Text::from_sections(sections),
-            ..Default::default()
-        }
-    }
+	/// Create a [`TextBundle`] from a list of sections.
+	///
+	/// See [`Text::from_sections`] for usage.
+	pub fn from_sections(sections: impl IntoIterator<Item = TextSection>) -> Self {
+		Self {
+			text: Text::from_sections(sections),
+			..Default::default()
+		}
+	}
 
-    /// Returns this [`TextBundle`] with a new [`TextAlignment`] on [`Text`].
-    pub const fn with_text_alignment(mut self, alignment: TextAlignment) -> Self {
-        self.text.alignment = alignment;
-        self
-    }
+	/// Returns this [`TextBundle`] with a new [`TextAlignment`] on [`Text`].
+	pub const fn with_text_alignment(mut self, alignment: TextAlignment) -> Self {
+		self.text.alignment = alignment;
+		self
+	}
 
-    /// Returns this [`TextBundle`] with a new [`Style`].
-    pub const fn with_style(mut self, style: Style) -> Self {
-        self.style = style;
-        self
-    }
+	/// Returns this [`TextBundle`] with a new [`Style`].
+	pub const fn with_style(mut self, style: Style) -> Self {
+		self.style = style;
+		self
+	}
 }
 
 impl Default for TextBundle {
