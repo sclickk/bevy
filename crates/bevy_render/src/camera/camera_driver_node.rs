057ea6526c3e0a47b1a45cb1eb21375724ac5b3e
Eric Villarreal <35571755+sclickk@users.noreply.github.com>
2022-07-22 15:05:09 -0500

    Simplify `CameraDriverNode::run`

--- crates/bevy_render/src/camera/camera_driver_node.rs
+++ crates/bevy_render/src/camera/camera_driver_node.rs
@@ -85,22 +85,21 @@ impl Node for CameraDriverNode {
 
 			#[cfg(feature = "trace")]
 			let _span = bevy_utils::tracing::info_span!("no_camera_clear_pass").entered();
-			let pass_descriptor = RenderPassDescriptor {
-				label: Some("no_camera_clear_pass"),
-				color_attachments: &[Some(RenderPassColorAttachment {
-					view: swap_chain_texture,
-					resolve_target: None,
-					ops: Operations {
-						load: LoadOp::Clear(wgpu::Color::BLACK),
-						store: true,
-					},
-				})],
-				depth_stencil_attachment: None,
-			};
 
 			render_context
 				.command_encoder
-				.begin_render_pass(&pass_descriptor);
+				.begin_render_pass(&RenderPassDescriptor {
+					label: Some("no_camera_clear_pass"),
+					color_attachments: &[Some(RenderPassColorAttachment {
+						view: swap_chain_texture,
+						resolve_target: None,
+						ops: Operations {
+							load: LoadOp::Clear(wgpu::Color::BLACK),
+							store: true,
+						},
+					})],
+					depth_stencil_attachment: None,
+				});
 		}
 
 		Ok(())
