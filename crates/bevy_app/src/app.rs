469a50837eaa2aefd83f05ea6fab39593bcd989e
Eric Villarreal <35571755+sclickk@users.noreply.github.com>
2022-07-24 12:56:17 -0500

    move `define_label` macro into `bevy_ecs`

--- crates/bevy_app/src/app.rs
+++ crates/bevy_app/src/app.rs
@@ -15,7 +15,7 @@ use std::fmt::Debug;
 
 #[cfg(feature = "trace")]
 use bevy_utils::tracing::info_span;
-bevy_utils::define_label!(
+bevy_ecs::define_label!(
 	/// A strongly-typed class of labels used to identify an [`App`].
 	AppLabel,
 	/// A strongly-typed identifier for an [`AppLabel`].
